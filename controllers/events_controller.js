bc541aee08a29632a14fdac8b09d776f37d62326
Khalid <Alsaoork@gmail.com>
2024-05-21 14:38:47 +0300

    Creating the index route for events controller

--- controllers/events_controller.js
+++ controllers/events_controller.js
@@ -3,5 +3,15 @@ const events = require("express").Router();
 const db = require("../models");
 const { Event } = db;
 
+// FIND ALL EVENTS
+events.get("/", async (req, res) => {
+  try {
+    const foundEvents = await Event.findAll();
+    res.status(200).json(foundEvents);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 // EXPORT
 module.exports = events;
