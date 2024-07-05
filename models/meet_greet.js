d6112e218e04fbee511a6ef5e2cc7410c90363b2
Khalid <Alsaoork@gmail.com>
2024-07-05 08:05:07 +0300

    sequelize associations

--- models/meet_greet.js
+++ models/meet_greet.js
@@ -2,13 +2,12 @@
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Meet_greet extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    static associate({ Band }) {
+      // band
+      Meet_greet.belongsTo(Band, {
+        foreignKey: "band_id",
+        as: "band",
+      });
     }
   }
   Meet_greet.init(
